867da74121f350a10400cf2b49092d8f3569b857
James Kao <james@jameskao.me>
2015-09-20 11:01:25 -0700

    structuring data w/pandas

--- key-stats.py
+++ key-stats.py
@@ -9,7 +9,7 @@ path = '/Users/James/Dropbox/Projects/sklearn/intraQuarter'
 def Key_Stats(gather = "Total Debt/Equity (mrq)"):
     statspath = path + '/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]
-    # print stock_list
+    df = pd.DataFrame(columns = ['Date', 'Unix', 'Ticker', 'DE Ratio'])
 
     for stock_dir in stock_list[1:]:
         stock_files = os.listdir(stock_dir)
@@ -18,21 +18,20 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
             for stock_file in stock_files:
                 date_stamp = datetime.strptime(stock_file, '%Y%m%d%H%M%S.html')
                 unix_time = time.mktime(date_stamp.timetuple())
-                # # time stamp
-                # print date_stamp, unix_time
-
                 stock_file_path = stock_dir + '/' + stock_file
-                print stock_file_path
-
                 source = open(stock_file_path, 'r').read()
-                # print source
+                try:
+                    value = float(source.split(gather + ':</td>')[1].split('<td class="yfnc_tabledata1">')[1].split('</td>')[0])
+                    df = df.append({'Date'     : date_stamp,
+                                    'Unix'     : unix_time,
+                                    'Ticker'   : ticker,
+                                    'DE Ratio' : value},
+                                   ignore_index = True)
+                except Exception as e:
+                    pass
 
-                if gather + ':</td>' in source:
-                    value = source.split(gather + ':</td>')[1].split('<td class="yfnc_tabledata1">')[1].split('</td>')[0]
-                else:
-                    value = 'N/A'
-                print ticker + ':' + value
-
-            time.sleep(15)
+    save = gather.replace(' ','').replace('(','').replace(')','').replace('/','') + '.csv'
+    print(save)
+    df.to_csv(save)
 
 Key_Stats()
