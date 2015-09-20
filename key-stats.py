82d5dbf78bb51d1d81c3b3b07aaae35983204257
James Kao <james@jameskao.me>
2015-09-20 12:45:23 -0700

    getting more data and meshing data sets

--- key-stats.py
+++ key-stats.py
@@ -10,6 +10,8 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
     statspath = path + '/_KeyStats'
     stock_list = [x[0] for x in os.walk(statspath)]
     df = pd.DataFrame(columns = ['Date', 'Unix', 'Ticker', 'DE Ratio'])
+    # Taken from https://www.quandl.com/data/YAHOO/INDEX_GSPC-S-P-500-Index
+    sp500_df = pd.DataFrame.from_csv('YAHOO-INDEX_GSPC.csv')
 
     for stock_dir in stock_list[1:]:
         stock_files = os.listdir(stock_dir)
@@ -22,10 +24,24 @@ def Key_Stats(gather = "Total Debt/Equity (mrq)"):
                 source = open(stock_file_path, 'r').read()
                 try:
                     value = float(source.split(gather + ':</td>')[1].split('<td class="yfnc_tabledata1">')[1].split('</td>')[0])
+
+                    try:
+                        sp500_date = datetime.fromtimestamp(unix_tim).strftime('%Y-%m-%d')
+                        row = sp500_df[sp500_df.index == sp500_date]
+                        sp500_value = float(row['Adjusted Close'])
+                    except: # Weekends / Single Day Holidays (-3 days)
+                        sp500_date = datetime.fromtimestamp(unix_time - 259200).strftime('%Y-%m-%d')
+                        row = sp500_df[sp500_df.index == sp500_date]
+                        sp500_value = float(row['Adjusted Close'])
+
+                    stock_price = float(source.split('</small><big><b>')[1].split('</b></big>')[0])
+
                     df = df.append({'Date'     : date_stamp,
                                     'Unix'     : unix_time,
                                     'Ticker'   : ticker,
-                                    'DE Ratio' : value},
+                                    'DE Ratio' : value,
+                                    'Price'    : stock_price,
+                                    'SP500'    : sp500_value},
                                    ignore_index = True)
                 except Exception as e:
                     pass
